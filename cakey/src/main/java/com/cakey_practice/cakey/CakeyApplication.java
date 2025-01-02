c361705ab7db7d0013417bb67845e7629cce27ed
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2025-01-02 18:25:08 +0900

    initial commit

---
+++ cakey/src/main/java/com/cakey_practice/cakey/CakeyApplication.java
@@ -0,0 +1,13 @@
+package com.cakey_practice.cakey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeyApplication.class, args);
+	}
+
+}
---
+++ cakey/src/test/java/com/cakey_practice/cakey/CakeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakey_practice.cakey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
